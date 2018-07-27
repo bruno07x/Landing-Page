1a4d63ff3c8c2564c098a964a6fe2c407e9c6f03
Bruno <Bruno@starnettelecom.com>
2018-07-27 05:12:39 -0300

    Finalizacao mobile

--- index.php
+++ index.php
@@ -14,14 +14,15 @@
 
 <body>
     <!--NOTE: H1 com finalidade de ficar oculta para dar mais semântica ao projeto. -->
+    <!-- NOTE: ########## DOBRA 1 ########## -->
     <h1 class="nulo">Nike HyperAdapt</h1>
     <section class="background__dobra1 shadow__element--1dp">
         <!-- NOTE: Configuração de layout usando CSS grid da dobra1 do projeto -->
-        <div class="entradaDobra1">
+        <div class="entradaDobra1 p-t-4">
             <div class="entradaDobra1__logo">
                 <img src="_VIEW/assets/IMG/pattern/nike__icon-light.png">
             </div>
-            <div class="entradaDobra1__frase">
+            <div class="entradaDobra1__frase m-t-4">
                 <h1 class="tipografia__titulo cores__suporteWhite">Just do it!</h1>
             </div>
             <div class="entradaDobra1__botao shadow__element--8dp button">
@@ -31,34 +32,31 @@
         </div>
     </section>
 
+    <!-- NOTE: ########## DOBRA 2 ########## -->
     <section class="">
         <!-- NOTE: Configuração de layout usando CSS grid da dobra2 do projeto -->
-        <div class="especificacoesDobra2">
+        <div class="especificacoesDobra2 p-4">
             <div class="especificacoesDobra2__fraseMain">
                 <h1 class="tipografia__titulo alinhamento__titulos cores__suporteGray">Eis aqui uma nova concepção de calçado!</h1>
             </div>
             <ul class="especificacoesDobra2__funcionalidades margin__elemento--1">
                 <!-- TODO: Adição de títulos e Alt para acessibilidade -->
-                <li class="tipografia__corpoTexto cores__suporteGray">Se ajusta automaticamente aos pés
+                <li class="tipografia__corpoTexto cores__suporteGray p-1">Ajuste automatico
                     <img src="_VIEW/assets/IMG/section2/especDobra2__ajuste-baseDark.png" alt="">
                 </li>
-                <li class="tipografia__corpoTexto cores__suporteGray">Controle de iluminação RGB
+                <li class="tipografia__corpoTexto cores__suporteGray p-1">iluminação RGB
                     <img src="_VIEW/assets/IMG/section2/especDobra2__color-baseDark.png" alt="">
                 </li>
-                <li class="tipografia__corpoTexto cores__suporteGray">Contador de passos e calorias
+                <li class="tipografia__corpoTexto cores__suporteGray p-1">Contador de passos
                     <img src="_VIEW/assets/IMG/section2/especDobra2__contagem-baseDark.png" alt="">
                 </li>
-                <li class="tipografia__corpoTexto cores__suporteGray">E mais opções no app de smartphone
+                <li class="tipografia__corpoTexto cores__suporteGray p-1">Mais opções no app
                     <img src="_VIEW/assets/IMG/section2/especDobra2__smartphone-baseDark.png"
                         alt="">
                 </li>
             </ul>
-            <div class="especificacoesDobra2__observacoes">
-                <p class="cores__suporteGray tipografia__corpoTexto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat imperdiet feugiat. Nunc mi nisl.Lorem
-                    ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat imperdiet feugiat. Nunc mi nisl.</p>
-            </div>
             <div class="especificacoesDobra2__exemploTenis alinhamento__elementos">
-                <img src="_VIEW/assets/IMG/section2/especDobra2__exemploTenis.png" alt="[Tênis Nike]" title ="Tênis Nike com funcionalidades">
+                <img class="image__recize" src="_VIEW/assets/IMG/section2/especDobra2__exemploTenis.png" alt="[Tênis Nike]" title ="Tênis Nike com funcionalidades">
             </div>
             <div class="especificacoesDobra2__fraseSecundaria">
                 <h3 class="tipografia__titulo alinhamento__titulos cores__suporteGray">Tecnologia e conforto na sola dos pés</h3>
@@ -66,70 +64,75 @@
         </div>
     </section>
 
+    <!-- NOTE: ########## DOBRA 3 ########## -->
     <section class="background__corBase">
         <div class="callDobra3">
             <div class="callDobra3__destaqueImagem">
-                <img class="" src="_VIEW/assets/IMG/section3/callDobra3__destaqueImagem.png" alt="Calçado nike">
+                <img class="image__recize" src="_VIEW/assets/IMG/section3/callDobra3__destaqueImagem.png" alt="Calçado nike">
             </div>
-            <div class="callDobra3__frase">
+            <div class="callDobra3__frase m-t-1">
                 <h1 class="tipografia__titulo cores__suporteWhite">Faça parte desta evolução!</h1>
             </div>
-            <div class="callDobra3__botao button">
+            <div class="callDobra3__botao button shadow__element--8dp m-b-4">
                 <a class="tipografia__subTitulo cores__suporteGray">Reserve</a>
             </div>
         </div>
     </section>
+
+    <!-- NOTE: ########## DOBRA 4 ########## -->
     <section>
-        <div class="perguntasDobra4">
+        <div class="perguntasDobra4 p-y-4">
             <div class="perguntasDobra4_frase">
-                <p class="tipografia__titulo cores__suporteGray">Perguntas frequentes:</p>
+                <p class="tipografia__titulo cores__suporteGray alinhamento__titulos">Perguntas frequentes:</p>
             </div>
             <div class="perguntasDobra4_pergunta1">
-                <p class="perguntasDobra4__backgroundPrimario">→ O que acompanha o produto?</p>
-                    <p class="perguntasDobra4__backgroundSecudaria">O produto vem acompanhado com 2 pares de carregadores com plug magnético, um manual de inicio rápido
+                <p class="perguntasDobra4__backgroundPrimario tipografia__subTitulo--segunda cores__suporteWhite">O que acompanha o produto?</p>
+                    <p class="perguntasDobra4__backgroundSecudaria tipografia__corpoTexto cores__suporteWhite">O produto vem acompanhado com 2 pares de carregadores com plug magnético, um manual de inicio rápido
                         com QR code para baixar o app</p>
             </div>
             <div class="perguntasDobra4_pergunta1">
-                <p class="perguntasDobra4__backgroundPrimario">→ Como controlo a iluminação do calçado?</p>
-                    <p class="perguntasDobra4__backgroundSecudaria">Através do app você consegue configurar a cor com o código hexadecimal ou simplesmente arrastando
+                <p class="perguntasDobra4__backgroundPrimario tipografia__subTitulo--segunda cores__suporteWhite">Como controlo a iluminação do calçado?</p>
+                    <p class="perguntasDobra4__backgroundSecudaria tipografia__corpoTexto cores__suporteWhite">Através do app você consegue configurar a cor com o código hexadecimal ou simplesmente arrastando
                         para o lado da cor favorita.</p>
             </div>
             <div class="perguntasDobra4_pergunta2">
-                <p class="perguntasDobra4__backgroundPrimario">→ Por outro lado, a competitividade nas transações?</p>
-                    <p class="perguntasDobra4__backgroundSecudaria">Gostaria de enfatizar que o consenso sobre a necessidade de qualificação agrega Gostaria de enfatizar
+                <p class="perguntasDobra4__backgroundPrimario tipografia__subTitulo--segunda cores__suporteWhite">Por outro lado, a competitividade nas transações?</p>
+                    <p class="perguntasDobra4__backgroundSecudaria tipografia__corpoTexto cores__suporteWhite">Gostaria de enfatizar que o consenso sobre a necessidade de qualificação agrega Gostaria de enfatizar
                         que o consenso sobre a necessidade de qualificação agrega .</p>
             </div>
             <div class="perguntasDobra4_pergunta2">
-                <p class="perguntasDobra4__backgroundPrimario">→ Valor ao estabelecimento dos métodos?</p>
-                    <p class="perguntasDobra4__backgroundSecudaria">Gostaria de enfatizar que o consenso sobre a necessidade de qualificação agrega Gostaria de enfatizar
+                <p class="perguntasDobra4__backgroundPrimario tipografia__subTitulo--segunda cores__suporteWhite">Valor ao estabelecimento dos métodos?</p>
+                    <p class="perguntasDobra4__backgroundSecudaria tipografia__corpoTexto cores__suporteWhite">Gostaria de enfatizar que o consenso sobre a necessidade de qualificação agrega Gostaria de enfatizar
                         que o consenso sobre a necessidade de qualificação agrega .</p>
             </div>
         </div>
     </section>
-    <!-- <section>
-        <div class="grid-full footer">
-            <div class="A-logo-secao5 m-y-0">
-                <img class="logo-footer" src="IMG/nike_icon.png">
+
+    <!-- NOTE: ########## FOOTER ########## -->
+    <section class="background__corBase">
+        <div class="footer p-y-4">
+            <div class="footer__icon">
+                <img class="" src="_VIEW/assets/IMG/pattern/nike__icon-light.png">
             </div>
-            <ul class="A-redes-sociais-footer text-center">
-                <li>
+            <ul class="footer__redesSociais m-t-4">
+                <li class="">
                     <a href="https://www.instagram.com/nike/?hl=pt-br">
-                        <img src="IMG/insta.png" alt="Instagram Nike">
+                        <img src="_VIEW/assets/IMG/footer/footer__instagram.png" alt="Instagram Nike">
                     </a>
                 </li>
-                <li class="m-x-1">
+                <li class="p-x-4">
                     <a href="https://pt-br.facebook.com/nike/">
-                        <img src="IMG/facebook.png" alt="Facebook Nike">
+                        <img src="_VIEW/assets/IMG/footer/footer__facebook.png" alt="Facebook Nike">
                     </a>
                 </li>
-                <li>
+                <li class="">
                     <a href="https://twitter.com/nike">
-                        <img src="IMG/twitter.png" alt="Twitter Nike">
+                        <img src="_VIEW/assets/IMG/footer/footer__twitter.png" alt="Twitter Nike">
                     </a>
                 </li>
             </ul>
         </div>
-    </section> -->
+    </section>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
